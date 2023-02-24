f834c9cfdeee7771b2c7ea58121d2ec498c275fe
Italo Modesto Pereira <italo.pereira@virtus.ufcg.edu.br>
2023-02-24 11:45:05 -0300

    Loader added to app

--- MoviesListApp/src/App.tsx
+++ MoviesListApp/src/App.tsx
@@ -2,38 +2,43 @@ import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   View,
-  FlatList
+  FlatList,
+  ActivityIndicator
 } from 'react-native';
 
 import Movie from './components/Movie';
 import api from './services/api';
 
-type MovieProps = {
-  id: number,
-  nome: string,
-  sinopse: string,
-  foto: any,
-}
-
 function App(): JSX.Element {
   const [movies, setMovies] = useState();
+  const [loading, setLoading] = useState(true)
   
   useEffect(() => { 
     async function loadMovies(){
       const resp = await api.get("r-api/?api=filmes");
       setMovies(resp.data);
+      setLoading(false);
     }
     loadMovies();
   }, []);
-  
-  return (
-    <View>
-      <FlatList
-      data={movies}
-      keyExtractor={ item => String(item.id)}
-      renderItem={ ({ item }) => <Movie data={item} />}/>
-    </View>
-  );
+
+  if(loading){
+    return(
+      <View style={{alignItems: "center", justifyContent: "center", flex: 1}}>
+        <ActivityIndicator color="#121212" size={45}/>
+      </View>
+    );
+  }
+  else{
+    return (
+      <View>
+        <FlatList
+        data={movies}
+        keyExtractor={ item => String(item.id)}
+        renderItem={ ({ item }) => <Movie data={item} />}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
